b5d2271a795aeca10ab48a979434e317c7139648
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-06-06 14:15:32 +0300

    cmd/scrum-report: add support of debug

--- cmd/scrum-report/main.go
+++ cmd/scrum-report/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"net"
+
+	"github.com/pkg/errors"
 
 	"github.com/oleg-balunenko/scrum-report/config"
 	"github.com/oleg-balunenko/scrum-report/logger"
@@ -12,10 +15,37 @@ func main() {
 	printVersion()
 
 	cfg := config.Load()
-
+	if cfg.Debug {
+		ip, err := getIP()
+		if err != nil {
+			log.Fatalf("failed to get ip: %v", err)
+		}
+		cfg.Host = ip
+	}
 	logger.SetUp(cfg)
 	r := reporter.New(cfg)
 
 	log.Fatal(r.Run())
+
+}
+
+func getIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	var ip string
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get addresses")
+	}
+
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ip = ipnet.IP.String()
+				break
+			}
+
+		}
+	}
+	return ip, nil
 
 }
