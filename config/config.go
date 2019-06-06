4a7c7745a5cb5f831fd193b34639e7dff6295f4d
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-06-06 14:15:10 +0300

    config: add debug mode

--- config/config.go
+++ config/config.go
@@ -9,15 +9,18 @@ type Config struct {
 	LogLevel    string
 	Port        string
 	Host        string
+	Debug       bool
 	OpenBrowser bool
 }
 
 // Load configuration from flags
 func Load() *Config {
 	var c Config
-	flag.StringVar(&c.Host, "host_address", "localhost", "address of host")
+	flag.StringVar(&c.Host, "host_address", "127.0.0.1", "address of host")
 	flag.StringVar(&c.Port, "listen_port", "8080", "listen port")
 	flag.StringVar(&c.LogLevel, "log_level", "INFO", "log level")
+	flag.BoolVar(&c.Debug, "debug", false, `Run debug mode - use real ip machine instead localhost 
+	to possible to debug with Charles`)
 	flag.BoolVar(&c.OpenBrowser, "open_browser", false, "open browser after start on index page")
 
 	flag.Parse()
