3ff74492973a1edfe85458dd1df866de4ef2c4dc
Oleg Balunenko <oleg.balunenko@gmail.com>
2019-09-02 17:18:12 +0300

    Update linters.sh

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -103,7 +103,7 @@ function golangci(){
         golangci-lint run ./...
     else
         printf "Cannot check golang-ci, please run:
-        curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.16.0 \n"
+        curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin \n"
         exit 1
     fi
     echo ""
