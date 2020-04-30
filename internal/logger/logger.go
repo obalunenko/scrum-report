4168d7075eb1cd28a3e1e0af50fd76e557f22b12
Oleg Balunenko <oleg.balunenko@gmail.com>
2020-04-30 15:47:56 +0300

    Move packages to internal

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -5,7 +5,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/oleg-balunenko/scrum-report/config"
+	"github.com/oleg-balunenko/scrum-report/internal/config"
 )
 
 // SetUp sets up logger logs level and format
