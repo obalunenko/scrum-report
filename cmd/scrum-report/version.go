6afd656474dfcc0bb5955daaf455546b4fbd628e
Oleg Balunenko <oleg.balunenko@gmail.com>
2020-11-03 18:51:20 +0300

    chore(cmd/scrum-report): Add build info

--- cmd/scrum-report/version.go
+++ cmd/scrum-report/version.go
@@ -5,14 +5,20 @@ import "fmt"
 const unset = "unset"
 
 var ( // build info
-	version = unset
-	date    = unset
-	commit  = unset
+	version     = unset
+	date        = unset
+	commit      = unset
+	committedBy = unset
+	builtBy     = unset
+	goVersion   = unset
 )
 
 func printVersion() {
-	fmt.Printf("Version info: %s \n", version)
+	fmt.Printf("version info: %s \n", version)
 	fmt.Printf("Build date: %s \n", date)
 	fmt.Printf("commit: %s \n", commit)
+	fmt.Printf("committed by: %s \n", committedBy)
+	fmt.Printf("built by: %s \n", builtBy)
+	fmt.Printf("go verion: %s \n", goVersion)
 	fmt.Println()
 }
