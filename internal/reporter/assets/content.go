2f3f1514f69581049a4893fc498c2204776c7c05
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-02-18 16:27:56 +0300

    refactor(assets): Use go 1.16 embed

---
+++ internal/reporter/assets/content.go
@@ -0,0 +1,36 @@
+// Package assets handles generated static.
+// To update views - add changes to template htmls.
+package assets
+
+import (
+	"embed"
+	"path/filepath"
+)
+
+const (
+	dir = "templates"
+)
+
+// content holds our puzzles inputs content.
+//go:embed templates/*
+var content embed.FS
+
+// Asset loads and returns the asset for the given name.
+// It returns an error if the asset could not be found or
+// could not be loaded.
+func Asset(name string) ([]byte, error) {
+	return content.ReadFile(filepath.Clean(
+		filepath.Join(dir, name)))
+}
+
+// MustAsset loads and returns the asset for the given name.
+// It panics if the asset could not be found or
+// could not be loaded.
+func MustAsset(name string) []byte {
+	res, err := Asset(name)
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
